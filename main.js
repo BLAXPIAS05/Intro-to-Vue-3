5bf710c12c2bf2602651e08fbb16fea373c31709
BLAXPIA05 <reaganw762@gmail.com>
2023-10-30 11:45:35 -0600

    initiate application

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+
+            description: 'These provide a barrier between your feet and shoes'
+        }
+    }
+})
